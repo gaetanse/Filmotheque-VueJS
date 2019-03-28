6f5dd9568f3f356c27013b6d8598f04adc850dcf
Subleader <baker.leader@gmail.com>
2019-03-28 15:46:27 +0100

    Nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
-  <nav>
-    <router-link to="/"><button type="button" class="btn btn-primary">Liste des films</button></router-link>
-    <router-link to="/favoris"><button type="button" class="btn btn-warning">Favoris</button></router-link>
-  </nav>
-  <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link to="/"><a class="nav-link" href="#">Liste des films <span class="sr-only">(current)</span></a></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/favoris"><a class="nav-link" href="#">Favoris</a></router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 </div>
 </template>
 
