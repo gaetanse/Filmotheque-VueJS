f0c5ee3af0d50e40be9ac9ade6b6bc9d83522283
Subleader <baker.leader@gmail.com>
2019-03-28 16:07:19 +0100

    Ajustement CSS

--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="liste">
-    <div class="holder">
-      <hr>
-      <form @submit.prevent="addItem">
-        <div class="input">
-          <label for="name">Tapez le nom du film (ou qzdqd ou qzddqz ou dwzdzq) :</label>
-          <input type="text" name="nom" class="form-control" v-model="nom" v-validate="'alpha'">
-          <span v-show="errors.has('nom')" class="alert">{{ errors.first('nom') }}</span>
+    <div class="liste">
+        <div class="row align-items-center" style="height: 100%">
+            <div class="col-md-7 mx-auto">
+                <hr>
+                <form @submit.prevent="addItem">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Rechercher un film</span>
+                        </div>
+                        <input type="text" class="form-control" v-model="nom" v-validate="'alpha'"
+                               placeholder="Tapez le nom du film" aria-label="Username" aria-describedby="basic-addon1">
+                        <span v-show="errors.has('nom')" class="alert">{{ errors.first('nom') }}</span>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
-  </div>
-  </div>
+    </div>
 </template>
 
 <style>
-  .input {
-    box-shadow: 0 0 5px rgba(0, 0, 0, 1);
-    padding: 3px 0px 3px 3px;
-    margin: 5px 1px 3px 0px;
-    border: 1px solid rgba(0, 0, 0, 1);
-  }
 </style>
