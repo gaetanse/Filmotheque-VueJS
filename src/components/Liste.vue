02a8be405148f144bcf44972b5bdfcdfeca1ab2f
gaetanse <gs99@hotmail.fr>
2019-03-28 16:36:04 +0100

    number

--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -4,23 +4,16 @@
       <hr>
       <form @submit.prevent="addItem">
         <div class="input">
-          <label>Tapez le nom du film (ou qzdqd ou qzddqz ou dwzdzq) :</label>
-          <input type="text" name="number" class="form-control" v-model="number" v-validate="'alpha'">
+          <label for="name">Tapez le nom du film (ou qzdqd ou qzddqz ou dwzdzq) :</label>
+          <input type="text" name="nom" class="form-control" v-model="nom" v-validate="'alpha'">
+          <span v-show="errors.has('nom')" class="alert">{{ errors.first('nom') }}</span>
         </div>
       </form>
-      <hr>
-
-      <div id="app">
-        <ul>
-          <p v-for="data in jsonData">{{data}}</p>
-        </ul>
-      </div>
   </div>
   </div>
 </template>
 
 <style>
-
   .input {
     box-shadow: 0 0 5px rgba(0, 0, 0, 1);
     padding: 3px 0px 3px 3px;
@@ -28,19 +21,3 @@
     border: 1px solid rgba(0, 0, 0, 1);
   }
 </style>
-
-<script>
-  import axios from 'axios';
-  export default {
-    data() {
-      return {
-        jsonData: [],
-        number: 299537,
-        url: "https://api.themoviedb.org/3/movie/299537?api_key=1d853ccc3f76e0d7e6544802f27005df&language=fr-FR",
-      }},
-    mounted() {
-      axios.get(this.url)
-              .then(response => (this.jsonData = response))
-    }
-  }
-</script>
