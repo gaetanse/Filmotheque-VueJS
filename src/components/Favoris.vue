430cdc3ba772fe53b71963977341c36900a9e243
Subleader <baker.leader@gmail.com>
2019-04-04 13:24:07 +0200

    Fix

--- src/components/Favoris.vue
+++ src/components/Favoris.vue
@@ -2,30 +2,30 @@
     <div class="favoris">
         <hr>
         <h1>Page de vos films favoris</h1>
-    <div class="row align-items-center" style="height: 100%">
-        <h5 v-if="tabFavoris">
-            <div class="card text-center" v-for="(data, index) in tabFavoris" :key='index'>
-                <div class="card-header">
-                    <h5 class="card-title">{{ data['data']['title'] }}</h5>
+        <div class="row align-items-center" style="height: 100%">
+            <h5 v-if="tabFavoris">
+                <div class="card text-center" v-for="(data, index) in tabFavoris" :key='index'>
+                    <div class="card-header">
+                        <h5 class="card-title">{{ data['data']['title'] }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <img class="card-img-top" v-bind:src="'https://image.tmdb.org/t/p/w500/'+data['data']['poster_path']" style="width: 30%;">
+                        <hr>
+                        <p class="card-text">{{ data['data']['overview'] }}</p>
+                    </div>
+                    <div class="card-footer text-muted">
+                        {{ data['data']['release_date']}}
+                    </div>
                 </div>
-                <div class="card-body">
-                    <img class="card-img-top" src="'https://image.tmdb.org/t/p/w500/'+data['poster_path']" style="width: 30%;">
-                    <hr>
-                    <p class="card-text">{{ data['data']['overview'] }}</p>
-                </div>
-                <div class="card-footer text-muted">
-                    {{ data['data']['release_date']}}
-                </div>
-            </div>
-        </h5>
-        <h5 v-else>Vos favoris sont vides </h5>
-        <button type="button" class="close" aria-label="Close">
-        </button>
+            </h5>
+            <h5 v-else>Vos favoris sont vides </h5>
+            <button type="button" class="close" aria-label="Close">
+            </button>
+        </div>
     </div>
-    </div>
-        </template>
+</template>
 
-        <script>
+<script>
     export default {
         name: 'Favoris',
         data() {
@@ -35,8 +35,8 @@
             }},
         computed: {
             tabFavoris : function () {
-                console.log(this.$store.getters.laListeFavoris);
-                return this.$store.getters.laListeFavoris;
+                console.log(JSON.parse(localStorage.getItem("listeFavoris")));
+                return JSON.parse(localStorage.getItem("listeFavoris"));
             }
         },
         props:{
