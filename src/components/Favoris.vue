68bc8c48f23050fe7801fcdbac50196c52f16a2e
Subleader <baker.leader@gmail.com>
2019-04-04 15:05:33 +0200

    Début de l'ajout des modal pour évaluer/commenter + fix de ;

--- src/components/Favoris.vue
+++ src/components/Favoris.vue
@@ -9,7 +9,6 @@
             <tr>
                 <th scope="col">Titre</th>
                 <th scope="col">Résumé</th>
-                <th scope="col">Commentaire</th>
                 <th scope="col">Evaluation</th>
                 <th scope="col">Supprimer</th>
             </tr>
@@ -18,12 +17,28 @@
             <tr>
                 <th scope="row">{{ data['data']['title'] }}</th>
                 <td>{{ data['data']['overview'] }}</td>
-                <td>manger</td>
-                <td>test</td>
-                <td><button v-on:click="remove(data['id'])" class="btn btn-primary">Supprimer</button></td>
+                <td>
+                    <b-button class="btn btn-primary" v-b-modal.modal-prevent>Evaluer</b-button>
+                </td>
+                <td>
+                    <button v-on:click="remove(data['id'])" class="btn btn-danger">Supprimer</button>
+                </td>
             </tr>
             </tbody>
         </table>
+
+        <!-- Modal Component -->
+        <b-modal
+                id="modal-prevent"
+                ref="modal"
+                title="Submit your name"
+                @ok="handleOk"
+                @shown="clearName"
+        >
+            <form @submit.stop.prevent="handleSubmit">
+                <b-form-input v-model="name" placeholder="Enter your name"></b-form-input>
+            </form>
+        </b-modal>
     </div>
 </template>
 
@@ -32,22 +47,48 @@
         name: 'Favoris',
         data() {
             return {
+                name: '',
+                names: [],
                 leFavori: [],
                 url: "",
-            }},
+            }
+        },
         computed: {
-            tabFavoris : function () {
+            tabFavoris: function () {
                 console.log(JSON.parse(localStorage.getItem("listeFavoris")));
                 return JSON.parse(localStorage.getItem("listeFavoris"));
             }
         },
-        props:{
+        props: {
             numbers: Array
         },
         methods: {
             remove(i) {
-                this.$store.commit('DELETE_FAVORI',i);
+                this.$store.commit('DELETE_FAVORI', i);
                 this.$router.go();
+            },
+            addEval(data) {
+
+            },
+            clearName() {
+                this.name = ''
+            },
+            handleOk(evt) {
+                // Prevent modal from closing
+                evt.preventDefault();
+                if (!this.name) {
+                    alert('Please enter your name')
+                } else {
+                    this.handleSubmit()
+                }
+            },
+            handleSubmit() {
+                this.names.push(this.name);
+                this.clearName();
+                this.$nextTick(() => {
+                    // Wrapped in $nextTick to ensure DOM is rendered before closing
+                    this.$refs.modal.hide()
+                })
             }
         }
     }
