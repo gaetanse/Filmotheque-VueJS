84780fa485158e43ba84e3717bd5f46c1a47974c
gaetanse <gs99@hotmail.fr>
2019-04-05 10:56:42 +0200

    tests modification

--- tests/unit/UtilTab.spec.js
+++ tests/unit/UtilTab.spec.js
@@ -2,8 +2,20 @@ import UtilTab from '../../src/models/UtilTab.js'
 
 test('Exemple de test d\'une fonction', () => {
     const tab = ['testA', 'testB']
-    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testA</td>')
-    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testB</td>')
-})
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testA</td>');
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testB</td>');
+});
+
+test('Exemple de test d\'une fonction', () => {
+    const tab = ['testA', 'testB']
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testA</td>');
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testB</td>');
+});
+
+test('Exemple de test d\'une fonction', () => {
+    const tab = ['testA', 'testB']
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testA</td>');
+    expect(UtilTab.toHtmlTab(tab, 2)).toMatch('<td>testB</td>');
+});
 
 
